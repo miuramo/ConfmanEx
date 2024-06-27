062bd02e3ae00d25356e0f1d3a669278a741be51
motoki miura <miura@moto.qee.jp>
2024-06-27 18:25:21 +0900

    debug auto_role_member

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -22,14 +22,16 @@ class Setting extends Model
      * Settingの REVIEWER_MEMBER や PC_MEMBERをみて、自動でロールをわりあてる
      */
     public static function auto_role_member(){
-        $sets = Setting::where("name","like","%_MEMBER%")->where("valid",true)->get();
+        $sets = Setting::where("name","like","%_MEMBER")->where("valid",true)->get();
         foreach($sets as $set){
             $val = $set->value;
+            if (strlen($val)<2) continue;
             // role name
             $role_name = strtolower(explode("_",$set->name)[0]);
             $role = Role::findByIdOrName($role_name);
             // | で区切る
             $ary = explode("|",$val);
+            if (count($ary)<1) continue;
             foreach($ary as $name){
                 $tmpu = User::where("name",$name)->first();
                 if ($tmpu == null) continue;
