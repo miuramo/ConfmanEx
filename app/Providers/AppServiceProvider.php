b557ba94e09037fb158c4a8fe75f181cbbb6063d
motoki miura <miura@moto.qee.jp>
2024-08-20 18:15:39 +0900

    modosu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -64,7 +64,7 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') == "production"){ // App::environment(['production'])) {
             URL::forceScheme('https');
         }
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
         // App::setLocale('ja');
     }
 }
