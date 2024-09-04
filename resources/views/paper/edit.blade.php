327fb19836a8f2aa6b866b90d05b1a850339607f
motoki miura <miura@moto.qee.jp>
2024-09-04 19:06:59 +0900

    nowrap

--- resources/views/paper/edit.blade.php
+++ resources/views/paper/edit.blade.php
@@ -133,7 +133,7 @@
                         <table class="border-cyan-500 border-2">
                             @foreach ($koumoku as $k => $v)
                                 <tr class="{{ $loop->iteration % 2 === 1 ? 'bg-cyan-50' : 'bg-white dark:bg-cyan-100' }}">
-                                    <td class="px-2 py-1">{{ $v }}</td>
+                                    <td class="px-2 py-1 whitespace-nowrap">{{ $v }}</td>
                                     @if(strlen($paper->{$k})<2)
                                     <td class="px-2 py-1 text-red-600 font-bold" id="confirm_{{ $k }}">（未設定）</td>
                                     @else
