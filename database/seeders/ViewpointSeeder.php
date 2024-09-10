dff3171a1a7de4767db517c165a092fd1a507a76
motoki miura <miura@moto.qee.jp>
2024-09-10 12:36:39 +0900

    seeder

--- database/seeders/ViewpointSeeder.php
+++ database/seeders/ViewpointSeeder.php
@@ -18,7 +18,7 @@ class ViewpointSeeder extends Seeder
                 'orderint' => 1,
                 'name' => 'score',
                 'desc' => '総合点',
-                'content' => "5：採択、4：採択に近い、3：中立、2：不採択に近い、1：不採択\n:number : 1 : 5 : 3",
+                'content' => "5：採択、4：採択に近い、3：中立、2：不採択に近い、1：不採択\n; number ; 1 ; 5 ",
                 'weight' => 1,
                 'doReturn' => true,
                 // 'contentafter' => '',
@@ -28,7 +28,7 @@ class ViewpointSeeder extends Seeder
                 'orderint' => 2,
                 'name' => 'comment',
                 'desc' => '査読コメント',
-                'content' => "査読コメントは著者に返ります。\n:textarea : 60 : 5 : （著者に返ります）",
+                'content' => "査読コメントは著者に返ります。\n; textarea ; 60 ; 5 ; （著者に返ります）",
                 'doReturn' => true,
             ]);
             \App\Models\Viewpoint::factory()->create([
@@ -36,14 +36,14 @@ class ViewpointSeeder extends Seeder
                 'orderint' => 3,
                 'name' => 'suisen',
                 'desc' => '論文賞/デモポスター賞推薦',
-                'content' => "数字が大きい(5)ほうが「推薦したい」になります。\n:number : 1 : 5 : 3",
+                'content' => "数字が大きい(5)ほうが「推薦したい」になります。\n; number ; 1 ; 5 ",
             ]);
             \App\Models\Viewpoint::factory()->create([
                 'category_id' => $cat,
                 'orderint' => 4,
                 'name' => 'suisencom',
                 'desc' => '推薦コメント',
-                'content' => "推薦コメントは委員会向けです。\n:textarea : 60 : 5 : (委員会向け) ",
+                'content' => "推薦コメントは委員会向けです。\n;textarea ; 60 ; 5 ; (委員会向け) ",
             ]);
         }
         //
