f784e196643bda4a7a0a3904c68f6cc70a56a05c
motoki miura <miura@moto.qee.jp>
2024-09-13 10:56:33 +0900

    add unique to scores

---
+++ database/migrations/2024_09_13_105150_add_unique_to_scores.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('scores', function (Blueprint $table) {
+            $table->unique(['review_id', 'viewpoint_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('scores', function (Blueprint $table) {
+            $table->dropUnique(['review_id', 'viewpoint_id', 'user_id']);
+        });
+    }
+};
