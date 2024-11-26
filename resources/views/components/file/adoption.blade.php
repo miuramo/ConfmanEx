bdc57d58804193716aea1b5df829ab1484bb71fb
motoki miura <miura@moto.qee.jp>
2024-11-26 14:27:18 +0900

    d

--- resources/views/components/file/adoption.blade.php
+++ resources/views/components/file/adoption.blade.php
@@ -6,17 +6,17 @@
 @if ($file->paper->is_accepted_in_any_category())
     @if ($file->id == $file->paper->pdf_file_id)
         <span
-            class="mx-1 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-2 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
+            class="mx-0 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-1 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
             PDFとして収録予定
         </span>
     @elseif($file->id == $file->paper->img_file_id)
         <span
-            class="mx-1 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-2 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
+            class="mx-0 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-1 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
             IMGとして収録予定
         </span>
     @elseif($file->id == $file->paper->video_file_id)
         <span
-            class="mx-1 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-2 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
+            class="mx-0 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-1 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
             Videoとして収録予定
         </span>
         <div class="my-2"></div>
@@ -25,7 +25,7 @@
         </x-element.deletebutton>
     @elseif($file->id == $file->paper->altpdf_file_id)
         <span
-            class="mx-1 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-2 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
+            class="mx-0 sm:rounded-lg border-2 border-blue-600 bg-cyan-200 px-1 py-1 font-bold text-blue-600 text-sm dark:bg-cyan-400">
             ALTPDFとして収録予定
         </span>
     @endif
