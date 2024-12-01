c339c98ce2cc28a51cb7736b8198a53fd7b63501
motoki miura <miura@moto.qee.jp>
2024-12-01 22:41:28 +0900

    hide title

--- resources/views/annot/show.blade.php
+++ resources/views/annot/show.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:bg-slate-800 dark:text-slate-400">
-            {{ $apaper->paper->id_03d() }} {{ $apaper->paper->title }}
-        </h2>
-    </x-slot>
+    {{-- <x-slot name="header"> --}}
+        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:bg-slate-800 dark:text-slate-400"> --}}
+            {{-- {{ $apaper->paper->id_03d() }} {{ $apaper->paper->title }} --}}
+        {{-- </h2> --}}
+    {{-- </x-slot> --}}
     <div class="m-2">
         @for ($i = 1; $i <= $apaper->file->pagenum; $i++)
             @php
