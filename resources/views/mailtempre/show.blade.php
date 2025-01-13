25281611f82f392dc17a0f0999e89731637c975c
motoki miura <miura@moto.qee.jp>
2025-01-13 19:49:18 +0900

    d

--- resources/views/mailtempre/show.blade.php
+++ resources/views/mailtempre/show.blade.php
@@ -104,7 +104,7 @@
             <div class="bg-slate-100 py-2 px-4 font-bold text-sm flex justify-between">
                 Cc : {{ implode(' , ', $to_cc['cc']) }}
                 @isset($mt->cc)
-                    {{ str_replace(',', ' , ', $mt->cc) }}
+                    , {{ str_replace(',', ' , ', $mt->cc) }}
                 @endisset
                 <span class="text-slate-400 text-right">Cc</span>
             </div>
