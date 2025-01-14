27f323f1078f5428ea415e9ec020cc3e38d2849a
motoki miura <miura@moto.qee.jp>
2025-01-14 18:55:23 +0900

    no info

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -40,7 +40,7 @@ class Setting extends Model
                 if ($tmpu == null) continue;
                 if (!$role->containsUser($tmpu->id)){ // ふくまれていなければ
                     $tmpu->roles()->syncWithoutDetaching($role);
-                    info("auto_role_member {$name} {$role->name}");
+                    debug("auto_role_member {$name} {$role->name}");
                 }
             }
         }
