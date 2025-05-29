d72ae2202718cbb5d9414b21d236e4ebdf6ea98f
motoki miura <miura@moto.qee.jp>
2025-05-29 10:01:20 +0900

    info and warn

--- app/Console/Commands/CheckFailedJobs.php
+++ app/Console/Commands/CheckFailedJobs.php
@@ -31,9 +31,10 @@ class CheckFailedJobs extends Command
         $failedJobsCount = DB::table('failed_jobs')->count();
         if ($failedJobsCount > 0) {
             Mail::to(env('MAIL_BCC_ADDRESS','miuramo@gmail.com'))->send(new FailedJobsAlert($failedJobsCount));
-            info("Warning email sent to admin.");
+            $this->warn("Warning email sent to admin.");
         } else {
-            // info("No failed jobs found.");
+            Mail::to(env('MAIL_BCC_ADDRESS','miuramo@gmail.com'))->send(new FailedJobsAlert($failedJobsCount));
+            $this->info("No failed jobs found.");
         }
         //
     }
