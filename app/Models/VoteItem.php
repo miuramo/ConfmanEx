fa194d72b94df5761254c81ea694777dff9d1450
motoki miura <miura@moto.qee.jp>
2025-08-01 10:09:38 +0900

    do not  separate ippan and gakusei

--- app/Models/VoteItem.php
+++ app/Models/VoteItem.php
@@ -36,10 +36,12 @@ class VoteItem extends Model
         // 各カテゴリで、学生発表とそれ以外（一般発表）に分ける。
         // アンケートは4番、paper_id => valuestr をとっておく。
         // または、valuestr = 学生 のpaper_id 配列をとっておく。
-        $student_pids = EnqueteAnswer::where("enquete_id",4)->where("valuestr","学生")->orderBy("paper_id")
-        ->get()->pluck("paper_id")->toArray();
+        $student_pids = EnqueteAnswer::where("enquete_id", 4)->where("valuestr", "学生")->orderBy("paper_id")
+            ->get()->pluck("paper_id")->toArray();
         // info($student_pids);
-        foreach([1,2] as $catid){
+
+        // 1,2のカテゴリで、学生発表とそれ以外（一般発表）に分ける。
+        /*        foreach([1,2] as $catid){
             foreach(["一般"=>false,"学生"=>true] as $zoku=>$val){
                 $subs = Submit::where("category_id", $catid)->whereHas("accept", function($query) {
                     $query->where("judge", ">", 0);
@@ -62,18 +64,43 @@ class VoteItem extends Model
 
             }
         }
+            */
+        // 学生と一般を分けない
+        $cats = Category::whereIn("id", [1, 2])->orderBy("id")->pluck("name", "id")->toArray();
+        foreach ($cats as $catid => $catname) {
+            $subs = Submit::where("category_id", $catid)->whereHas("accept", function ($query) {
+                $query->where("judge", ">", 0);
+            })->orderBy("orderint")->select("paper_id", "booth")->pluck("paper_id", "booth")->toArray();
+            VoteItem::firstOrCreate(
+                [
+                    'vote_id' => $catid,
+                    'name' => "【{$catname}】",
+                ],
+                [
+                    'orderint' => 1,
+                    'submits' => json_encode($subs),
+                ]
+            );
+        }
+    }
 
+    // 学生発表のブースを取得
+    // booth => paper_id の配列を返す。
+    // 1,2のカテゴリで、学生発表とそれ以外（一般発表）に分ける。
+    // ただし、アンケートは4番、paper_id => valuestr をとっておく。
+    public static function init_boothes()
+    {
+        self::init();
     }
     public static function student_boothes()
     {
-        $student_pids = EnqueteAnswer::where("enquete_id",4)->where("valuestr","学生")->orderBy("paper_id")
-        ->get()->pluck("paper_id")->toArray();
-        $subs = Submit::whereHas("accept", function($query) {
+        $student_pids = EnqueteAnswer::where("enquete_id", 4)->where("valuestr", "学生")->orderBy("paper_id")
+            ->get()->pluck("paper_id")->toArray();
+        $subs = Submit::whereHas("accept", function ($query) {
             $query->where("judge", ">", 0);
-        })->whereHas("paper", function($query) use ($student_pids){
+        })->whereHas("paper", function ($query) use ($student_pids) {
             $query->whereIn("id", $student_pids);
-        })->orderBy("orderint")->select("paper_id","booth")->pluck("paper_id","booth")->toArray();
+        })->orderBy("orderint")->select("paper_id", "booth")->pluck("paper_id", "booth")->toArray();
         return $subs;
     }
-
 }
