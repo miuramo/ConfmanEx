e621cf25efb69604f87212920def61202ed536a8
motoki miura <miura@moto.qee.jp>
2025-08-05 16:56:41 +0900

    d

--- resources/views/livewire/man-user-norole.blade.php
+++ resources/views/livewire/man-user-norole.blade.php
@@ -1,7 +1,7 @@
 <div>
     {{ count($norole_users) }}件の役職なしユーザがいます。
     <div class="mt-4">
-        <input type="button" wire:click="softdelete_norole_nopaper_users" class="bg-orange-300 text-white text-sm hover:bg-orange-600 p-2 rounded-lg" value="役職なし＆発表なしのユーザをソフトデリート"/>
+        <input type="button" wire:click="softdelete_norole_nopaper_users" class="bg-orange-300 text-white text-sm hover:bg-orange-600 p-2 rounded-lg" value="役職なし＆発表なしのユーザを論理削除"/>
         <div class="flex items-center justify-between p-2 bg-gray-100 rounded mb-2">
             <ul>
                 @foreach ($norole_users as $u)
@@ -14,5 +14,4 @@
             </ul>
         </div>
     </div>
-    {{-- The Master doesn't talk, he acts. --}}
 </div>
