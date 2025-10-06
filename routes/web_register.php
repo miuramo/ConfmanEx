d291dc709e0e5f1406ff5a57d79c736d3752ea45
motoki miura <miura@moto.qee.jp>
2025-10-06 16:37:52 +0900

    regist_entry

--- routes/web_register.php
+++ routes/web_register.php
@@ -7,16 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::resource('regist', RegistController::class);
     Route::get('/regist_email/{regist}', [RegistController::class, 'email'])->name('regist.email');
-    // Route::get('/annot/{annot}/show/{page?}', [AnnotController::class, 'show'])->name('annot.showpage');
-    // Route::post('/annot_postsubmit', [AnnotController::class, 'postsubmit'])->name('annot.postsubmit');
-    // // Route::get('/annot_jsubmit/{annot}', [AnnotController::class, 'jsubmit'])->name('annot.submit');
-    // // Route::get('/jsubmit/{annot?}', [AnnotController::class, 'jsonsubmit'])->name('annot.jsonsubmit');
-    // Route::get('/annot/{annot}/comment_json/{page?}', [AnnotController::class, 'comment_json'])->name('annot.comment_json');
-
-    // Route::post('/annot/{annot}/setpublic', [AnnotController::class, 'setpublic'])->name('annot.setpublic');
-    Route::get('/regist_entry', [RegistController::class, 'entry'])->name('regist.entry');
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('/regist_entry', [RegistController::class, 'entry'])->name('regist.entry');
+
 });
+
+Route::get('/regist_entry', [RegistController::class, 'entry'])->name('regist.entry');
+
