5033cc47a3b9ad1dd221fbf971b2d1839dc0aae5
motoki miura <miura@moto.qee.jp>
2025-10-28 17:17:28 +0900

    pending registration

--- app/Livewire/RegistCheckAuthor.php
+++ app/Livewire/RegistCheckAuthor.php
@@ -16,12 +16,20 @@ class RegistCheckAuthor extends Component
 
     public $papers_without_presenters = [];
 
+    public $pending_pids = [];
+    public $pending_name = [];
+
     public function mount()
     {
         $this->finished = \App\Models\Regist::with('user')->where('valid', 1)->orderby('created_at')->get();
+        $pending = \App\Models\Regist::with('user')->where('valid', 0)->orderby('created_at')->get();
         $withauthor_paper_pids = [];
         foreach( $this->finished as $reg ) {
             $withauthor_paper_pids = array_merge($withauthor_paper_pids, $reg->user->accepted_papers_as_any());
+        }
+        foreach( $pending as $reg ) {
+            $this->pending_pids[$reg->user->id] = $reg->user->accepted_papers_as_any();
+            $this->pending_name[$reg->user->id] = $reg->user->name;
         }
 
         $this->accepted_paper_pids = Submit::with('paper')->whereHas("accept", function ($query) {
@@ -29,7 +37,6 @@ class RegistCheckAuthor extends Component
         })->orderby('category_id')->orderby('paper_id')->get()->pluck("paper_id")->toArray();
 
         $this->papers_without_presenters = array_diff($this->accepted_paper_pids, $withauthor_paper_pids);
-
     }
 
     public function render()
--- resources/views/livewire/regist-check-author.blade.php
+++ resources/views/livewire/regist-check-author.blade.php
@@ -6,6 +6,7 @@
             <th class="px-2">Cat</th>
             <th class="px-2">PID</th>
             <th class="px-2">Title</th>
+            <th class="px-2">Owner status</th>
         </tr>
         @foreach ($papers_without_presenters as $pid)
             @php
@@ -16,6 +17,24 @@
                 <td class="px-1 text-center">{{ $paper->category_id }}</td>
                 <td class="px-1 text-center">{{ $paper->id }}</td>
                 <td class="px-1 ">{{ $paper->title }}</td>
+                <td class="px-1 ">
+                    @isset($pending_name[$paper->owner])
+                    <span class="text-blue-400">
+                    {{ $pending_name[$paper->owner] }} ({{$paper->owner}}) 未完了
+                    </span>
+                    @else
+                    <span class="text-pink-400">
+                    @php
+                        $user = \App\Models\User::find($paper->owner);
+                    @endphp
+                    @if( $user )
+                        {{ $user->name }} ({{ $paper->owner }}) 未開始
+                    @else
+                        (未登録ユーザ)
+                    @endif 
+                    </span>
+                    @endisset
+                </td>
             </tr>
         @endforeach
     </table>
