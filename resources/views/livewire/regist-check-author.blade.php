75601ade3109924aa9c24acc5bb695f2fb79eeb7
motoki miura <miura@moto.qee.jp>
2025-10-28 17:39:37 +0900

    pick up userids

--- resources/views/livewire/regist-check-author.blade.php
+++ resources/views/livewire/regist-check-author.blade.php
@@ -1,6 +1,9 @@
 <div>
     <b class="text-emerald-600">発表者がまだ参加登録完了していない採択発表一覧</b> ({{ count($papers_without_presenters) }}件)
-
+    @php
+        $mikaishi = [];
+        $mikanryo = [];
+    @endphp
     <table class="text-sm border-collapse border border-slate-400 mb-4">
         <tr class="bg-slate-300">
             <th class="px-2">Cat</th>
@@ -19,24 +22,32 @@
                 <td class="px-1 ">{{ $paper->title }}</td>
                 <td class="px-1 ">
                     @isset($pending_name[$paper->owner])
-                    <span class="text-blue-400">
-                    {{ $pending_name[$paper->owner] }} ({{$paper->owner}}) 未完了
-                    </span>
+                        <span class="text-blue-400">
+                            {{ $pending_name[$paper->owner] }} ({{ $paper->owner }}) 未完了
+                        </span>
+                        @php
+                            $mikanryo[] = $paper->owner;
+                        @endphp
                     @else
-                    <span class="text-pink-400">
-                    @php
-                        $user = \App\Models\User::find($paper->owner);
-                    @endphp
-                    @if( $user )
-                        {{ $user->name }} ({{ $paper->owner }}) 未開始
-                    @else
-                        (未登録ユーザ)
-                    @endif 
-                    </span>
+                        <span class="text-pink-400">
+                            @php
+                                $user = \App\Models\User::find($paper->owner);
+                            @endphp
+                            @if ($user)
+                                {{ $user->name }} ({{ $paper->owner }}) 未開始
+                                @php
+                                    $mikaishi[] = $paper->owner;
+                                @endphp
+                            @else
+                                (未登録ユーザ)
+                            @endif
+                        </span>
                     @endisset
                 </td>
             </tr>
         @endforeach
     </table>
-
-</div>
+    <div class="mb-4 text-sm"> User IDs: 
+        <b class="text-blue-400 text-sm">{{ implode(', ', array_unique($mikanryo)) }}</b> <span class="mx-2"></span>
+        <b class="text-pink-400 text-sm">{{ implode(', ', array_unique($mikaishi)) }}</b>
+    </div>
