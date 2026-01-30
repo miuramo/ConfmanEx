8e591efe383aa46f0955c7bc89a7494e51f4c332
motoki miura <miura@moto.qee.jp>
2026-01-30 12:15:13 +0900

    test

--- database/factories/RegistFactory.php
+++ database/factories/RegistFactory.php
@@ -2,22 +2,37 @@
 
 namespace Database\Factories;
 
+use App\Models\Regist;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Regist>
- */
 class RegistFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Regist::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'valid' => $this->faker->boolean,
+            'paid' => $this->faker->boolean,
+            'paid_at' => $this->faker->optional()->dateTime,
+            'payment_method' => $this->faker->optional()->word,
+            'payment_id' => $this->faker->optional()->uuid,
+            'payment_status' => $this->faker->optional()->word,
+            'confirmed_at' => $this->faker->optional()->dateTime,
+            'isearly' => $this->faker->boolean,
+            'submitted_at' => $this->faker->dateTime,
         ];
     }
 }
