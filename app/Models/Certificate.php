2d0c06f7db7fb1584446fd113e0f8a24e8692469
motoki miura <miura@moto.qee.jp>
2026-08-17 18:04:34 +0900

    factory

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Certificate extends Model
 {
+    use HasFactory;
+
     //
 }
---
+++ database/factories/CertificateFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Certificate;
+use App\Models\Setting;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Certificate>
+ */
+class CertificateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'orderint' => 1,
+            'winner' => '',
+            'awardname' => '優秀論文賞',
+            'year' => Setting::getval('CONFTITLE_YEAR'),
+            'eventname' => '情報教育シンポジウム' . Setting::getval('CONFTITLE_YEAR'),
+            'creator' => '@IPSJ',
+            'company' => '情報処理学会',
+            'date' => '令和８年８月２１日',
+            'content' => '貴殿が[:company:][:eventname:]にて発表された[:title:]は特に優秀な論文であり、情報教育の発展に貢献することを認めここに表彰いたします。',
+            'presenter' => "[:date:]\r\n一般社団法人 [:company:]\r\n[:eventname:]\r\nプログラム委員長XX XX\r\n実行委員長YY YY\r\n大会委員長ZZ ZZ",
+            'template' => 'default',
+        ];
+    }
+}
