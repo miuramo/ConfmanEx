2aab06c3d3461c53612294aabbe5ca31533db21e
motoki miura <miura@moto.qee.jp>
2026-09-01 11:58:27 +0900

    enq ans sortable

--- resources/views/components/admin/enqtable_nopaper.blade.php
+++ resources/views/components/admin/enqtable_nopaper.blade.php
@@ -24,7 +24,7 @@
 @endphp
 <!-- components.admin.enqtable_nopaper -->
 
-<table class="min-w-full divide-y divide-gray-200 text-sm">
+<table class="min-w-full divide-y divide-gray-200 text-sm sortable" id="enqtable">
     <thead>
         <tr>
             @foreach ($heads as $h)
