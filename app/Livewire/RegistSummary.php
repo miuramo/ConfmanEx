e3f93ef8e6d8c1727e7041d998e5aa88b23ded80
motoki miura <miura@moto.qee.jp>
2026-09-04 16:35:57 +0900

    REGIST_SUMMARY_ITEMS

--- app/Livewire/RegistSummary.php
+++ app/Livewire/RegistSummary.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Setting;
 use Livewire\Component;
 
 class RegistSummary extends Component
@@ -16,11 +17,18 @@ class RegistSummary extends Component
         $this->finishedCount = \App\Models\Regist::where('valid', 1)->count();
         $this->notfinishedCount = \App\Models\Regist::where('valid', 0)->count();
 
+        // もしSetting::get
+        $reg_sum_items = Setting::getval("REGIST_SUMMARY_ITEMS");
+        $reg_sum_items = json_decode($reg_sum_items);
+        if (is_array($reg_sum_items)) {
+            $this->items = $reg_sum_items;
+        }
         $summary = [];
         foreach ($this->items as $itm) {
             $summary[$itm] = \App\Models\Regist::countByItemAndIsearly($itm);
         }
         $this->summary = $summary;
+
     }
 
     public function render()
